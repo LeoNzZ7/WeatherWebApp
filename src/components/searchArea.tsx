d2622ecf587b62bab419dfaa08e3a82a2185c359
Leonardo Nunes <leonardomartinha20019@gmail.com>
2022-05-13 19:10:14 -0300

    API configurada

--- src/components/searchArea.tsx
+++ src/components/searchArea.tsx
@@ -1,14 +1,41 @@
+import { API } from "./API"
+import { ChangeEvent, useState } from "react";
+import { CityCords } from "../types/types";
+
 export const SearchArea = () => {
+    const api = API;
+
+    const [location, setLocation] = useState('');
+    const [loading, setLoading] = useState(false);
+
+    const handleSearch = async () => {
+        setLoading(true);
+        const cords: CityCords | any = await api.getCityCords(location);
+        const getInfoNow = await api.getInfoNow(cords.coord.lat, cords.coord.lon);
+        setLoading(false);
+    }
+
+    const handleSearchInput = (e: ChangeEvent<HTMLInputElement>) => {
+        setLocation(e.target.value);
+    }
+
     return(
         <div className="h-full w-full flex flex-col justify-center items-center">
             <h1 className="text-white text-4xl">Buscar localização</h1>
             <div className="w-full mt-10 mb-10 flex justify-center">
-                <input type='text' className="p-2 rounded-tl-xl rounded-bl-xl text-white bg-slate-800 h-10 w-[375px] outline-none focus:opacity-90 hover:opacity-90"/>
-                <button className="p-2 w-[100px] h-10 border-l-2 border-slate-900 bg-slate-800 rounded-tr-xl rounded-br-xl text-white hover:opacity-90">Buscar</button>
+                <input 
+                type='text' 
+                value={location}
+                onChange={handleSearchInput}
+                className="p-2 rounded-tl-xl rounded-bl-xl text-white bg-slate-800 h-10 w-[375px] outline-none focus:opacity-90 hover:opacity-90"/>
+                <button 
+                onClick={handleSearch} 
+                className="p-2 w-[100px] h-10 border-l-2 border-slate-900 bg-slate-800 rounded-tr-xl rounded-br-xl text-white hover:opacity-90"
+                >Buscar</button>
             </div>
-            <div className="w-[475px] rounded-xl bg-slate-800">
-                
-            </div>
+            {loading &&
+                <h1 className="text-white text-center" >Buscando as informações metereologicas de {location}, por favor aguarde.</h1>
+            }
         </div>
     )
 }
---
+++ src/types/types.ts
@@ -0,0 +1,6 @@
+export type CityCords = {
+    coord: {
+        lon: string;
+        lat: string;
+    }
+}
